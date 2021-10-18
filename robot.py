be12feba43281c4b252255c63c6c938e876dc597
mmoss <mmoss020@student.wethinkcode.co.za>
2021-10-18 09:18:31 +0200

    Toy Robot 1 All Steps Complete

--- robot.py
+++ robot.py
@@ -1,48 +1,91 @@
 
+def move_forward(distance):
+    """
+    The robot will move forward by the distance specified
+    """
+    print(f"* Move Forward {distance}")
 
-# TODO: Decompose into functions
-def move():
-    size = 10
-    print("Moving in a square of size "+str(size))
-    for i in range(4):
-        degrees = 90
-        print("* Move Forward "+str(size))
-        print("* Turn Right "+str(degrees)+" degrees")
 
-    length = 20
-    width = 10
-    print("Moving in a rectangle of "+str(length)+" by "+str(width))
+def turn_right(degrees):
+    """
+    The robot will turn right by the amount
+    of degrees specified
+    """
+    print(f"* Turn Right {degrees} degrees")
+
+
+def move_square(size):
+    """
+    size decides the distance the robot will go
+    for each side of the square
+    """
+    move_rectangle(size, size)
+    
+
+def move_rectangle(length, width):
+    """
+    length and width decide the distance the robot will go
+    for the two sides of the rectangle. If they are equal,
+    they will move in a square 
+    """
+    if length != width:
+        print(f"Moving in a rectangle of {length} by {width}")
+    else:
+        print(f"Moving in a square of size {length}")
+    degrees = 90
     for i in range(2):
-        degrees = 90
-        print("* Move Forward "+str(length))
-        print("* Turn Right "+str(degrees)+" degrees")
-        print("* Move Forward "+str(width))
-        print("* Turn Right "+str(degrees)+" degrees")
+        move_forward(length)
+        turn_right(degrees)
+        move_forward(width)
+        turn_right(degrees)
 
+
+def move_circle(length = 1):
+    """
+    The robot moves in a circle with a circumference
+    of length multiplied by 360. By default length = 1
+    """
     print("Moving in a circle")
     degrees = 1
     for i in range(360):
-        length = 1
-        print("* Move Forward "+str(length))
-        print("* Turn Right "+str(degrees)+" degrees")
+        move_forward(length)
+        turn_right(degrees)
 
-    print("Square dancing - 3 squares of size 20")
+
+def move_square_dancing(length):
+    """
+    Will move forward and move in a square, repeat 3 times
+    length decides the size of the square and the distance
+    it will move forward
+    """
+    print(f"Square dancing - 3 squares of size {length}")
     for i in range(3):
-        length = 20
-        print("* Move Forward "+str(length))
-        print("Moving in a square of size 10")
-        for j in range(4):
-            print("* Move Forward " + str(size))
-            print("* Turn Right " + str(degrees) + " degrees")
-            degrees = 90
+        move_forward(length)
+        move_square(length)
 
+
+def move_crop_circles(length):
+    """
+    Will move forward and move in a circle, repeat 4 times
+    length decides how far it moves forward
+    """
     print("Crop circles - 4 circles")
     for i in range(4):
-        print("* Move Forward "+str(length))
-        print("Moving in a circle")
-        for k in range(360):
-            print("* Move Forward " + str(length))
-            print("* Turn Right " + str(degrees) + " degrees")
+        move_forward(length)
+        move_circle()
+
+
+def move():
+    """
+    This Goes through each preprogrammed 
+    movement pattern available to the 
+    robot.
+    """
+    move_square(size = 10)
+    move_rectangle(length = 20, width = 10)
+    move_circle()
+    move_square_dancing(length = 20)
+    move_crop_circles(length = 20)
 
 
 def robot_start():
